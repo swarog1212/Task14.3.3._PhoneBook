14acbd7d47329162677f572d705ba8a247f1d6b7
george <dark8609@gmail.com>
2021-12-29 17:29:13 +0200

    Задание 14.3.3.

---
+++ PhoneBook/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+            while (true)
+            {
+                var input = Console.ReadKey().KeyChar;
+                Console.Clear();
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine("Страница отсутстыует!");
+                }
+                else
+                {
+                    switch (pageNumber)
+                    {
+                        case 1:
+                            var pageContent = phoneBook.Take(2);
+                            var sort = pageContent.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+                            foreach (var item in sort)
+                            {
+                                Console.WriteLine(item.Name + " " + item.LastName + " - " + item.PhoneNumber + ", " + item.Email);
+                            }
+                            break;
+                        case 2:
+                            pageContent = phoneBook.Skip(2).Take(2);
+                            sort = pageContent.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+                            foreach (var item in sort)
+                            {
+                                Console.WriteLine(item.Name + " " + item.LastName + " - " + item.PhoneNumber + ", " + item.Email);
+                            }
+                            break;
+                        case 3:
+                            pageContent = phoneBook.Skip(4).Take(2);
+                            sort = pageContent.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+                            foreach (var item in sort)
+                            {
+                                Console.WriteLine(item.Name + " " + item.LastName + " - " + item.PhoneNumber + ", " + item.Email);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
